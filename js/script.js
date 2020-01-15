7ea0bb6ab596c4ce177c1b3f3227cf30121d087a
Thranoraqs <50324556+Thranoraqs@users.noreply.github.com>
2020-01-15 13:52:01 +0100

    jsfix

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $("#showSection").click(function(){
+	$("#sectionBox").removeClass("d-none");
+    $("#showSection").addClass("d-none");
+    $("#geen-content-message").addClass("d-none");
+  });
+});
