6e30ed0dff9a452fe9a50fcaedd6214d7be70731
Joel Kouthoofd <44805078+JoelKouthoofd@users.noreply.github.com>
2020-01-15 16:40:31 +0100

    Form Base

---
+++ login/login.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+?>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link href="login.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+
+    <div id="login">
+
+    </div>
+
+</body>
+</html>
