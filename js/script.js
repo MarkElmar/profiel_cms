3dbb60f4dfef659297238cdfd9666e7a27ca24f6
Thranoraqs <50324556+Thranoraqs@users.noreply.github.com>
2020-01-15 15:59:26 +0100

    form-create

--- js/script.js
+++ js/script.js
@@ -6,8 +6,37 @@ $(document).ready(function(){
   });
 });
 var prevVal;
-$("#inputTaste").on("change",function(){
-  var val = $(this).find('option:selected').val();
-  $(".content").removeClass(`content-${prevVal}`).addClass(`content-${val}`);
-  prevVal = val;
+$("#inputTemplate").on("change",function(){
+	var val = $(this).find('option:selected').val();
+	if(val == 00)
+	{
+		$("#alleen-tekst").removeClass("d-none");
+		$("#blok-tekst").addClass("d-none");
+		$("#links-tekst").addClass("d-none");
+		$("#rechts-tekst").addClass("d-none");
+	} else if(val == 01)
+	{
+		$("#alleen-tekst").addClass("d-none");
+		$("#blok-tekst").removeClass("d-none");
+		$("#links-tekst").addClass("d-none");
+		$("#rechts-tekst").addClass("d-none");
+	} else if(val == 02)
+	{
+		$("#alleen-tekst").addClass("d-none");
+		$("#blok-tekst").addClass("d-none");
+		$("#links-tekst").removeClass("d-none");
+		$("#rechts-tekst").addClass("d-none");
+	}else if(val == 03)
+	{
+		$("#alleen-tekst").addClass("d-none");
+		$("#blok-tekst").addClass("d-none");
+		$("#links-tekst").addClass("d-none");
+		$("#rechts-tekst").removeClass("d-none");
+	}else if(val == 99)
+	{
+		$("#alleen-tekst").addClass("d-none");
+		$("#blok-tekst").addClass("d-none");
+		$("#links-tekst").addClass("d-none");
+		$("#rechts-tekst").addClass("d-none");
+	}
 });	
