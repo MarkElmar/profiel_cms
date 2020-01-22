2139084ba4e9b6c317d468340b070e56e7f1a4c6
Mark Jansen <markelmar2002@gmail.com>
2020-01-22 13:23:27 +0100

    Merged Login_Front with Sign-in

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link href="css/login.css" type="text/css" rel="stylesheet">
+    <link href="css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
+</head>
+<body>
+<div id="login">
+    <div id="login-frame"></div>
+    <div id="sign-in">
+        <h2 style="font-size: 5vw">Register_</h2>
+        <input type="text">
+        <input type="text" class="form-c">
+    </div>
+</div>
+</body>
+</html>
