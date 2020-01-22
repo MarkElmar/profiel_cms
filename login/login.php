b4679f00d7083f3fe79dd6aaede002335b110e11
Joel Kouthoofd <44805078+JoelKouthoofd@users.noreply.github.com>
2020-01-22 13:18:32 +0100

    Form front

--- login/login.php
+++ login/login.php
@@ -9,11 +9,19 @@
     <meta charset="utf-8">
     <title>Login</title>
     <link href="login.css" type="text/css" rel="stylesheet">
+    <link href="bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat&display=swap" rel="stylesheet">
 </head>
 <body>
 
     <div id="login">
-
+        <div id="login-frame"></div>
+        <div id="sign-in">
+            <h2 style="font-size: 5vw">Register_</h2>
+            <input type="text">
+            <input type="text" class="form-c">
+            <
+        </div>
     </div>
 
 </body>
